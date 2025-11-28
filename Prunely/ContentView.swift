2e8d68dc79cc9c33b6b7aa8d0769da3edfc8c40a
Kevin Pan <kpan58911@gmail.com>
2025-11-28 00:02:04 -0500

    Initial Commit

---
+++ Prunely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prunely
+//
+//  Created by Kevin Pan on 11/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prunely/PrunelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrunelyApp.swift
+//  Prunely
+//
+//  Created by Kevin Pan on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PrunelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
